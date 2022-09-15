ee9510b27bed79855b713d5e696193fde174437c
education-my <education.mykhailo@gmail.com>
2022-09-15 21:08:17 +0300

    Module 3 - Final task

--- src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
+++ src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
@@ -2,5 +2,13 @@ package lang.print.gaps.finalModuleTask;
 
 public class DigitsSumCalculator {
     public void calculateSum(int number) {
+        int result = 0;
+        String numberString = number + "";
+        System.out.println(
+                Integer.parseInt("" + numberString.charAt(0))
+                        + Integer.parseInt("" + numberString.charAt(1))
+                        + Integer.parseInt("" + numberString.charAt(2))
+                        + Integer.parseInt("" + numberString.charAt(3))
+        );
     }
 }
--- src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
@@ -2,5 +2,7 @@ package lang.print.gaps.finalModuleTask;
 
 public class NumberReverter {
     public void revert(int number) {
+        String numberString = number + "";
+        System.out.println("" + numberString.charAt(2) + numberString.charAt(1) + numberString.charAt(0));
     }
 }
